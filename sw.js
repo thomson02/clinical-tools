71584da4543d657f163029b927c25b8a59df9b7a
Andrew Thomson <thomsoar@Andrews-MacBook-Pro.local>
2026-02-05 21:22:40 +0000

    Updates

---
+++ sw.js
@@ -0,0 +1,76 @@
+const CACHE_NAME = 'clinical-tools-v3'
+const CORE_ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.webmanifest',
+  '/logo.jpg',
+  '/icons/icon-192.svg',
+  '/icons/icon-512.svg',
+  '/logo.jpg',
+  '/splash/splash-640x1136.png',
+  '/splash/splash-750x1334.png',
+  '/splash/splash-828x1792.png',
+  '/splash/splash-1125x2436.png',
+  '/splash/splash-1170x2532.png',
+  '/splash/splash-1242x2688.png',
+  '/splash/splash-1284x2778.png',
+  '/tools/tools.json',
+  '/tools/frailty.json'
+]
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => cache.addAll(CORE_ASSETS))
+      .then(() => self.skipWaiting())
+  )
+})
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
+    )
+  )
+  self.clients.claim()
+})
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting()
+  }
+})
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return
+
+  const url = new URL(event.request.url)
+  if (url.origin !== self.location.origin) return
+
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request)
+        .then((response) => {
+          const clone = response.clone()
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone))
+          return response
+        })
+        .catch(() => caches.match(event.request).then((cached) => cached || caches.match('/')))
+    )
+    return
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      const fetchPromise = fetch(event.request).then((response) => {
+        if (response && response.status === 200) {
+          const clone = response.clone()
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone))
+        }
+        return response
+      })
+      return cached || fetchPromise
+    })
+  )
+})
